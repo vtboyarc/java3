8f1e9a7be21a56b4ada75fcc26716dd26fab0965
Adam Carter <adamrcarter@gmail.com>
2014-10-17 15:19:40 -0500

    followed heroku instructions to get a java app deployed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,7 +21,7 @@ public class Main extends HttpServlet {
 
   private void showHome(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    resp.getWriter().print("Hello from Java!");
+    resp.getWriter().print("Hello from Java! Yay, Java app deployed with Heroku. Cool, I guess...");
   }
 
   private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
